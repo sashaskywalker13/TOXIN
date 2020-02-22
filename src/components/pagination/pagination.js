cc86f04553e5ecf316a0fa012c015de62a2e7acf
sashaskywalker13 <53815710+sashaskywalker13@users.noreply.github.com>
2020-02-22 22:34:16 +0300

    datepicker

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
-	$('.pagination').pagination({
-	    dataSource: [1, 2, 3, 4],
-	    className: 'paginationjs-theme-blue',
-	    callback: function(data, pagination) {
-	        // template method of yourself
-	        var html = template(data);
-	        $('.pagination__list').html(html);
-	    }
-	})
-});
+// $(document).ready(function() {
+// 	$('.pagination').pagination({
+// 	    dataSource: [1, 2, 3, 4],
+// 	    className: 'paginationjs-theme-blue',
+// 	    callback: function(data, pagination) {
+// 	        // template method of yourself
+// 	        var html = template(data);
+// 	        $('.pagination__list').html(html);
+// 	    }
+// 	})
+// });
--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,13 @@ import '../components/input/__mask/input__mask.js';
 import '../components/dropdown/dropdown.js';
 import '../components/rangeSlider/rangeSlider.js';
 import '../components/pagination/pagination.js';
+import '../components/datepicker/datepicker.js';
 
 
 import '../../node_modules/inputmask/dist/jquery.inputmask.min.js';
 import '../../node_modules/ion-rangeslider/js/ion.rangeSlider.min.js';
 import '../../node_modules/paginationjs/dist/pagination.min.js';
+import '../../node_modules/air-datepicker/dist/js/datepicker.min.js'
+// import '../../node_modules/jquery-ui-multidatespicker/jquery-ui.multidatespicker.js';
+// import '../../node_modules/jquery-ui/external/jquery-custom/jquery.js';
+// import '../../src/static/jquery-ui.js';
